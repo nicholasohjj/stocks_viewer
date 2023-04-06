82a3c4b5f9a58ee2a0d6717f4f36c2b2abaaa6d5
nicholasohjj <nicholasohjj@gmail.com>
2023-04-06 09:32:15 +0800

    convert to pandas

--- index.py
+++ index.py
@@ -1,16 +1,50 @@
 import requests
 from dotenv import load_dotenv
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
+from mplfinance.original_flavor import candlestick_ohlc
+import matplotlib.dates as mdates
 import csv
 import os
 import time
+import datetime
 import json
+
 load_dotenv()
 
-api_key = os.getenv("api_key")
+ts = TimeSeries(key=os.getenv("api_key"),output_format="pandas")
+
 TIME_FRAMES = None
 with open("time_frames.json") as file:
     TIME_FRAMES = json.load(file)
+
+def get_data(symbol,ts_function, outputsize="compact"):
+    data, meta_data = ts_function(ts, symbol=symbol)
+    print(data)
+    data.index = pd.to_datetime(data.index)
+    return data
+
+def format_data(data, choice):
+    if choice == "1":
+        data = data.resample("1min").agg({
+            "1. open": "first",
+            "2. high": "max",
+            "3. low": "min",
+            "4. close": "last",
+            "5. volume": "sum"
+        })
+    elif choice == "2":
+        data = data[["1. open", "2. high", "3. low", "4. close"]]
+        data.columns = ["Open", "High", "Low", "Close"]
+    else:
+        data = data[["1. open", "2. high", "3. low", "4. close", "5. adjusted close"]]
+        data.columns = ["Open", "High", "Low", "Close", "Adjusted Close"]
+    data = data.reset_index()
+    data["date"] = data["date"].apply(mdates.date2num)
+    return data
+
 
 def lookup():
 
@@ -41,22 +75,16 @@ def lookup():
             else:
                 time_frame_chosen = option
 
-    url = TIME_FRAMES[time_frame_chosen]["url"]
-    url = url.replace("_API_KEY_",api_key)
-    url = url.replace("_SYMBOL_", symbol)
+    if time_frame_chosen == "1":
+        data = get_data(symbol,TimeSeries.get_intraday,outputsize="compact")
+    elif time_frame_chosen == "2":
+        data = get_data(symbol.TimeSeries.get_daily_adjusted,outputsize="compact")
+    elif time_frame_chosen == "3":
+        data = get_data(symbol,TimeSeries.get_weekly_adjusted)
+    elif time_frame_chosen == "4":
+        data = get_data(symbol,TimeSeries.get_monthly_adjusted)
 
-    response = requests.get(url)
-    raw_data = response.json()
-    print(raw_data)
-    if len(raw_data) == 1:
-        print("Invalid symbol, please try again.")
-        return lookup()
-    data_index = [i for i in raw_data.keys()][1]
-    data = raw_data[data_index]
-
-    header = TIME_FRAMES[time_frame_chosen]["time"]
-
-    return symbol,data,header
+    return data,time_frame_chosen,symbol
 
 ## TO-DO modify writing into csv to fit all options
 def write_into_csv(symbol,data, header):
@@ -74,31 +102,33 @@ def write_into_csv(symbol,data, header):
             contents = [date] + [values[detail] for detail in details]
             writer.writerow(contents)
 
-def read_csv_and_plot(symbol):
-    dates = []
-    closing_prices = []
-    title = ""
-    with open(f"{symbol}.csv") as csvfile:
-        reader = csv.DictReader(csvfile)
-        print(title)
-        for row in reader:
-            if not title:
-                title = list(row.keys())[-1]
-            dates.append(row["Date"])
-            closing_prices.append(float(row["Close"]))
-
-    plt.plot(dates, closing_prices)
-    plt.title(f"{title} Closing Prices for {symbol}")
-    plt.xlabel("Date")
-    plt.ylabel("Closing Price ($)")
-    plt.show()
+def display_data(data, time_frame_chosen,symbol):
+    if time_frame_chosen == "1":
+        fig, ax = plt.subplots()
+        candlestick_ohlc(ax, data.values, width=0.0005, colorup="g", colordown="r")
+        ax.xaxis_date()
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+        plt.title(f"Intraday Candlestick Chart for {symbol}")
+        plt.xlabel("Date")
+        plt.ylabel("Price ($)")
+        plt.show()
+    else:
+        plt.plot(data["date"], data["Close"])
+        fig, ax = plt.subplots()
+        ax.xaxis_date()
+        candlestick_ohlc(ax, data.values, width=0.0005, colorup="g", colordown="r")
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
+        plt.title(time_frame_chosen + " Stock Data for " + symbol)
+        plt.xlabel("Date")
+        plt.ylabel("Price ($)")
+        plt.show()
 
 
 print("Stocks Visualiser")
 print("========")
 time.sleep(0.5)
-symbol,data,header = lookup()
-write_into_csv(symbol,data,header)
-read_csv_and_plot(symbol)
+data,time_frame_chosen,symbol = lookup()
+data = format_data(data, time_frame_chosen)
+display_data(data, time_frame_chosen,symbol)
 
 
