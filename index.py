6f09ea1995b7ad9ff1a34b09783e1394754bfd9e
nicholasohjj <nicholasohjj@gmail.com>
2023-04-06 15:31:20 +0800

    integration with plotly

--- index.py
+++ index.py
@@ -1,15 +1,14 @@
-import requests
 from dotenv import load_dotenv
-from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 from mplfinance.original_flavor import candlestick_ohlc
-import matplotlib.dates as mdates
-import csv
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import os
 import time
-import datetime
 import json
 
 load_dotenv()
@@ -24,27 +23,9 @@ def get_data(symbol,ts_function, outputsize="compact"):
     data, meta_data = ts_function(ts, symbol=symbol)
     print(data)
     data.index = pd.to_datetime(data.index)
-    return data
+    data = data.sort_index(ascending=True)
 
-def format_data(data, choice):
-    if choice == "1":
-        data = data.resample("1min").agg({
-            "1. open": "first",
-            "2. high": "max",
-            "3. low": "min",
-            "4. close": "last",
-            "5. volume": "sum"
-        })
-    elif choice == "2":
-        data = data[["1. open", "2. high", "3. low", "4. close"]]
-        data.columns = ["Open", "High", "Low", "Close"]
-    else:
-        data = data[["1. open", "2. high", "3. low", "4. close", "5. adjusted close"]]
-        data.columns = ["Open", "High", "Low", "Close", "Adjusted Close"]
-    data = data.reset_index()
-    data["date"] = data["date"].apply(mdates.date2num)
     return data
-
 
 def lookup():
 
@@ -78,57 +59,29 @@ def lookup():
     if time_frame_chosen == "1":
         data = get_data(symbol,TimeSeries.get_intraday,outputsize="compact")
     elif time_frame_chosen == "2":
-        data = get_data(symbol.TimeSeries.get_daily_adjusted,outputsize="compact")
+        data = get_data(symbol,TimeSeries.get_daily_adjusted,outputsize="compact")
     elif time_frame_chosen == "3":
         data = get_data(symbol,TimeSeries.get_weekly_adjusted)
     elif time_frame_chosen == "4":
         data = get_data(symbol,TimeSeries.get_monthly_adjusted)
-
-    return data,time_frame_chosen,symbol
+    time_frame_header = TIME_FRAMES[time_frame_chosen]["time"]
+    return data,time_frame_chosen,symbol,time_frame_header
 
 ## TO-DO modify writing into csv to fit all options
-def write_into_csv(symbol,data, header):
 
-    with open(f"{symbol}.csv", "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        details = list(list(data.values())[0].keys())
-        row_title = ["Date"]
-        for detail in details:
-            title = detail.split()[1].capitalize()
-            row_title.append(title)
-        row_title.append(header)
-        writer.writerow(row_title)
-        for date, values in data.items():
-            contents = [date] + [values[detail] for detail in details]
-            writer.writerow(contents)
-
-def display_data(data, time_frame_chosen,symbol):
-    if time_frame_chosen == "1":
-        fig, ax = plt.subplots()
-        candlestick_ohlc(ax, data.values, width=0.0005, colorup="g", colordown="r")
-        ax.xaxis_date()
-        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
-        plt.title(f"Intraday Candlestick Chart for {symbol}")
-        plt.xlabel("Date")
-        plt.ylabel("Price ($)")
-        plt.show()
-    else:
-        plt.plot(data["date"], data["Close"])
-        fig, ax = plt.subplots()
-        ax.xaxis_date()
-        candlestick_ohlc(ax, data.values, width=0.0005, colorup="g", colordown="r")
-        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
-        plt.title(time_frame_chosen + " Stock Data for " + symbol)
-        plt.xlabel("Date")
-        plt.ylabel("Price ($)")
-        plt.show()
+def display_data(data, header,symbol):
+    fig = go.Figure(data=[go.Candlestick(x=data.index,
+                open=data['1. open'], high=data['2. high'],
+                low=data['3. low'], close=data['4. close'])
+                     ])
+    fig.update_layout(title=f'{symbol} Stock Price ({time_frame_header})', xaxis_rangeslider_visible=False)
+    fig.show()
 
 
 print("Stocks Visualiser")
 print("========")
 time.sleep(0.5)
-data,time_frame_chosen,symbol = lookup()
-data = format_data(data, time_frame_chosen)
-display_data(data, time_frame_chosen,symbol)
+data,time_frame_chosen,symbol,time_frame_header = lookup()
+display_data(data, time_frame_header,symbol)
 
 
